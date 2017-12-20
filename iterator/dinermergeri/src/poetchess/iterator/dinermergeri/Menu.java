cd9f3a0ef7eeb3d19e41344c134dc137b8bbc80e
poetchess <ilvbaicai@126.com>
2017-12-20 16:13:59 +0800

    iterator design pattern: using iterator interface provided by java.util

---
+++ iterator/dinermergeri/src/poetchess/iterator/dinermergeri/Menu.java
@@ -0,0 +1,10 @@
+package poetchess.iterator.dinermergeri;
+
+import java.util.Iterator;
+
+/**
+ * Created by poetchess on 17-12-20.
+ */
+public interface Menu {
+    Iterator<MenuItem> createIterator();
+}
---
+++ iterator/dinermergeri/src/poetchess/iterator/dinermergeri/MenuTest.java
@@ -0,0 +1,29 @@
+package poetchess.iterator.dinermergeri;
+
+/**
+ * Created by poetchess on 17-12-20.
+ */
+public class MenuTest {
+    public static void main(String[] args) {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+        waitress.printMenu();
+        waitress.printVegetarianMenu();
+
+        System.out.println("\nCustomer asks, is the Hotdog vegetarian?");
+        System.out.print("Waitress says: ");
+        if (waitress.isItemVegetarian("Hotdog")) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        System.out.println("\nCustomer asks, are the Waffles vegetarian?");
+        System.out.print("Waitress says: ");
+        if (waitress.isItemVegetarian("Waffles")) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
